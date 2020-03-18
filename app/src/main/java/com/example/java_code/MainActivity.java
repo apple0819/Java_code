77b87f8bac536f5d272e2fed21ca48c507671157
apple0819 <apple0819@users.noreply.github.com>
2020-03-18 10:42:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/java_code/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.java_code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        1) 이벤트 설정
+//        2) 초기 화면 값 세팅 .setText 등
+//        + main 함수는 가능한한 적은 코드만 남기자.
+//         => onCreate 메쏘드도 가능한 깔끔하게 정리.
+        
+        setupEvents();
+        setValues();
+
+    }
+
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+
+}
